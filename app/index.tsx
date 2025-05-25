4a95b77d099eaf6905ee072df7dde41557280e19
boiverde <aguiar.neves@hotmail.com>
2025-05-25 20:25:03 -0300

    Adicionar logo ao projeto

--- app/index.tsx
+++ app/index.tsx
@@ -37,7 +37,7 @@ export default function SplashScreen() {
       
       <Animated.View style={[styles.content, { opacity: fadeAnim }]}>
         <Image 
-          source={{ uri: 'https://images.pexels.com/photos/267559/pexels-photo-267559.jpeg' }}
+          source={require('@/assets/images/logo.png')}
           style={styles.logo}
         />
         <Text style={styles.title}>Bênção Match</Text>
